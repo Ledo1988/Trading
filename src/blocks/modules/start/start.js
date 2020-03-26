800b6ca8eec539152fb0f6b780e4927fab3bef74
Elena <elena.do1988@yandex.ru>
2020-03-26 15:34:05 +0300

    start is finished

--- src/blocks/modules/start/start.js
+++ src/blocks/modules/start/start.js
@@ -3,10 +3,23 @@ import $ from "jquery";
 
 $(document).ready(function() {
     var swiper = new Swiper('.start__slider-content', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
         pagination: {
             el: '.swiper-pagination',
             type: 'fraction',
+            formatFractionCurrent: function(number) {
+                if (number < 10) {
+                    number = '0' + number;
+                }
+                return number;
+            },
+            renderFraction: function (currentClass, totalClass) {
+                return '<span class="' + currentClass + '"></span>';
+            },
         },
+
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
